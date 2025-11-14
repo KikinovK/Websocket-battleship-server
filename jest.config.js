13ed592e517767b3443f51549488e9fb61eea38e
Kostiantyn Kikinov <kikinov.k@gmail.com>
2025-11-14 15:28:22 +0200

    feat: add workflow

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+export default {
+  preset: 'ts-jest/presets/default-esm',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/src/__tests__'],
+  extensionsToTreatAsEsm: ['.ts'],
+  transform: {
+    '^.+\\.ts$': ['ts-jest', { useESM: true }],
+  },
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+  },
+};
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+// Entry point for the application
+console.log('Hello from index.ts!');
+
+// Export default empty object to make this a module
+export default {};
